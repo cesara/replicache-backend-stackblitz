a5f803b6292f5ad5ee56a469515c8c4941b81c21
Gregory Baker <greg@roci.dev>
2022-10-27 13:50:48 -0700

    fix: use ECMAScript module rather than CommonJS, define process.env.NODE_ENV

--- static/index.js
+++ static/index.js
@@ -1,32 +1,31 @@
 import {
   Replicache,
   TEST_LICENSE_KEY,
-} from "https://unpkg.com/replicache@11.2.0/out/replicache.js";
-
+} from "https://unpkg.com/replicache@11.2.0/out/replicache.mjs";
 
 function log(msg) {
-  document.querySelector('#log').value += msg + '\n';
+  document.querySelector("#log").value += msg + "\n";
 }
 
 const rep = new Replicache({
-  name: 'New Replicache',
+  name: "New Replicache",
   licenseKey: TEST_LICENSE_KEY,
   mutators: {
     increment: async (tx, delta) => {
-      const prev = (await tx.get('count')) ?? 0;
+      const prev = (await tx.get("count")) ?? 0;
       const next = prev + delta;
-      await tx.put('count', next);
+      await tx.put("count", next);
     },
   },
 });
 
-rep.subscribe(async (tx) => (await tx.get('count')) ?? 0, {
+rep.subscribe(async (tx) => (await tx.get("count")) ?? 0, {
   onData: (count) => log(`count changed: ${count}`),
 });
 
-document.querySelector('#increment').onclick = () => {
+document.querySelector("#increment").onclick = () => {
   rep.mutate.increment(1);
-  log('incremented');
+  log("incremented");
 };
 
 async function helloReplicache() {
