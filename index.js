062e10ad3463df539de9de82ca16fcede26d8eff
Cesar Alaestante <cesara@gmail.com>
2022-10-27 11:44:18 -0700

    update to replicache-express

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express = require('express');
-const app = express();
 const port = 3010;
-const path = require('path');
+import { ReplicacheExpressServer } from "replicache-express";
+import express from 'express';
+import path from 'path';
+const mutators = {
+  increment: async (tx, delta) => {
+    const prev = (await tx.get('count')) ?? 0;
+    const next = prev + delta;
+    await tx.put('count', next);
+  },
+}
 
-app.use(express.static('static'));
+const options = {
+  mutators,
+  port,
+  host: process.env.HOST || '0.0.0.0',
+};
 
-app.get('/', (req, res) => {
+const r = new ReplicacheExpressServer(options);
+r.app.get('/', (req, res) => {
   res.sendFile(path.resolve('pages/index.html'));
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+r.app.use(express.static('static'));
+r.start(() => {
+  console.log(
+    `Replicache is listening on ${options.host}:${options.port}`,
+  );
 });
