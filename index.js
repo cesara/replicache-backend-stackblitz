f6128a40453f7c75f41c86f9cd3e7d0dab17f775
Cesar Alaestante <cesara@gmail.com>
2022-10-27 17:41:11 -0700

    working clean

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 const port = 3010;
-import { ReplicacheExpressServer } from "replicache-express";
+import { ReplicacheExpressServer } from 'replicache-express';
 import express from 'express';
 import path from 'path';
 import fs from 'fs';
+import fetch from 'node-fetch';
 const mutators = {
   increment: async (tx, delta) => {
     const prev = (await tx.get('count')) ?? 0;
     const next = prev + delta;
     await tx.put('count', next);
   },
-}
+};
 
 const options = {
   mutators,
   port,
   host: process.env.HOST || '0.0.0.0',
 };
-
+let resolveSpaceID;
+let spaceID = new Promise((resolve) => {
+  resolveSpaceID = resolve;
+});
 const r = new ReplicacheExpressServer(options);
+r.app.use(['/api/push', '/api/pull', '/api/poke'], async (req, res) => {
+  req.url = req.baseUrl.replace('/api/', '/api/replicache/');
+  req.query.spaceID = await spaceID;
+  r.app.handle(req, res);
+});
 r.app.use(express.static('static'));
 r.app.use('*', (_req, res) => {
-  const index = path.join("pages", 'index.html');
+  const index = path.join('pages', 'index.html');
   const html = fs.readFileSync(index, 'utf8');
-  res.status(200).set({'Content-Type': 'text/html'}).end(html);
+  res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
 });
-r.start(() => {
-  console.log(
-    `Replicache is listening on ${options.host}:${options.port}`,
+r.start(async () => {
+  console.log(`Replicache is listening on ${options.host}:${options.port}`);
+  const resp = await fetch(
+    `http://${options.host}:${options.port}/api/replicache/createSpace`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
   );
+  const respJSON = await resp.json();
+  console.log('createdSpace', respJSON);
+  resolveSpaceID(respJSON.spaceID);
 });
--- static/index.js
+++ static/index.js
@@ -1,93 +1,47 @@
 import {
   Replicache,
   TEST_LICENSE_KEY,
-} from "https://unpkg.com/replicache@11.2.0/out/replicache.mjs";
+} from 'https://unpkg.com/replicache@11.2.0/out/replicache.mjs';
 
-async function main() {
-  const { pathname } = window.location;
-
-  if (pathname === "/" || pathname === "") {
-    window.location.href = "/space/" + (await createSpace());
-    return;
-  }
-
-  // URL layout is "/space/<spaceid>"
-  const paths = pathname.split("/");
-  const [, spaceDir, spaceID] = paths;
-  if (
-    spaceDir !== "space" ||
-    spaceID === undefined ||
-    !(await spaceExists(spaceID))
-  ) {
-    window.location.href = "/";
-    return;
-  }
-
-  function log(msg) {
-    document.querySelector("#log").value += msg + "\n";
-  }
-
-  const rep = new Replicache({
-    name: "New Replicache",
-    licenseKey: TEST_LICENSE_KEY,
-    pushURL: `/api/replicache/push?spaceID=${spaceID}`,
-    pullURL: `/api/replicache/pull?spaceID=${spaceID}`,
-    mutators: {
-      increment: async (tx, delta) => {
-        const prev = (await tx.get("count")) ?? 0;
-        const next = prev + delta;
-        await tx.put("count", next);
-      },
-    },
-  });
-
-  // Implements a Replicache poke using Server-Sent Events.
-  // If a "poke" message is received, it will pull from the server.
-  const ev = new EventSource(`/api/replicache/poke?spaceID=${spaceID}`, {
-    withCredentials: true,
-  });
-  ev.onmessage = async (event) => {
-    if (event.data === "poke") {
-      await rep.pull();
-    }
-  };
-
-  rep.subscribe(async (tx) => (await tx.get("count")) ?? 0, {
-    onData: (count) => log(`count changed: ${count}`),
-  });
-
-  document.querySelector("#increment").onclick = () => {
-    rep.mutate.increment(1);
-    log("incremented");
-  };
-
-
-  async function spaceExists(spaceID) {
-    return await fetchJSON("spaceExists", spaceID);
-  }
-
-  async function createSpace(spaceID) {
-    const createSpaceRes = await fetchJSON("createSpace", spaceID);
-    if (createSpaceRes) {
-      return createSpaceRes.spaceID;
-    }
-  }
-
-  async function fetchJSON(apiName, spaceID) {
-    const res = await fetch(`/api/replicache/${apiName}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body:
-        spaceID &&
-        JSON.stringify({
-          spaceID,
-        }),
-    });
-    return await res.json();
-  }
-
-  log(`Hello from Replicache client: ${await rep.clientID}`);
+function log(msg) {
+  const textarea = document.querySelector('#log');
+  textarea.scrollTop = textarea.scrollHeight;
+  textarea.value += msg + '\n';
 }
-await main();
+
+
+const rep = new Replicache({
+  name: 'New Replicache',
+  licenseKey: TEST_LICENSE_KEY,
+  pushURL: `/api/push`,
+  pullURL: `/api/pull`,
+  mutators: {
+    increment: async (tx, delta) => {
+      const prev = (await tx.get('count')) ?? 0;
+      const next = prev + delta;
+      await tx.put('count', next);
+    },
+  },
+});
+
+// Implements a Replicache poke using Server-Sent Events.
+// If a "poke" message is received, it will pull from the server.
+const ev = new EventSource(`/api/poke`, {
+  withCredentials: true,
+});
+ev.onmessage = async (event) => {
+  if (event.data === 'poke') {
+    await rep.pull();
+  }
+};
+
+rep.subscribe(async (tx) => (await tx.get('count')) ?? 0, {
+  onData: (count) => log(`count changed: ${count}`),
+});
+
+document.querySelector('#increment').onclick = () => {
+  rep.mutate.increment(1);
+  log('incremented');
+};
+
+log(`Hello from Replicache client: ${await rep.clientID}`);
